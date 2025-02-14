016220e8133c7179a54eef163f9d351577cdb9aa
CarlosJavier <carlos.valenzuela.j92@gmail.com>
2025-02-14 19:51:15 +0100

    Commit - apiController hoteles

--- src/main/java/com/example/retoapirest/controller/ApiController.java
+++ src/main/java/com/example/retoapirest/controller/ApiController.java
@@ -1,11 +1,21 @@
 package com.example.retoapirest.controller;
 
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.retoapirest.model.Hotel;
+import com.example.retoapirest.repository.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
+
+    @Autowired
+    HotelRepository hotelRepository;
+
+    @GetMapping("/")
+    public List<Hotel> all(){
+        return hotelRepository.findAll();
+    }
 
 }
--- src/main/java/com/example/retoapirest/repository/HotelRepository.java
+++ src/main/java/com/example/retoapirest/repository/HotelRepository.java
@@ -4,4 +4,6 @@ import com.example.retoapirest.model.Hotel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface HotelRepository extends MongoRepository<Hotel, String> {
+
+
 }
