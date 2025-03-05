4f1d19359bbad978c6f4c31dbae98553697934d7
CarlosJavier <carlos.valenzuela.j92@gmail.com>
2025-03-05 16:40:17 +0100

    Commit - webController endpoint listado hoteles v1

--- src/main/java/com/example/retoapirest/controller/WebController.java
+++ src/main/java/com/example/retoapirest/controller/WebController.java
@@ -1,10 +1,27 @@
 package com.example.retoapirest.controller;
 
 
+import com.example.retoapirest.repository.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RestController("")
+@RequestMapping("/web")
 public class WebController {
+
+    @Autowired
+    HotelRepository hotelRepository;
+
+    @GetMapping("/")
+    public String index(Model model){
+        var hoteles = hotelRepository.findAll();
+        model.addAttribute("titulo", "Listado de Hoteles");
+        model.addAttribute("hoteles", hoteles);
+        return "index";
+    }
+
 }
