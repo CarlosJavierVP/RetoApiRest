093788a55915cf2d86541ac1a9b5a42127a931cc
CarlosJavier <carlos.valenzuela.j92@gmail.com>
2025-03-05 21:20:58 +0100

    Commit - webController javadoc

--- src/main/java/com/example/retoapirest/controller/WebController.java
+++ src/main/java/com/example/retoapirest/controller/WebController.java
@@ -13,18 +13,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Clase WebController
+ * Pasa los datos de la BD a una plantilla html
+ */
 @Controller
 @RequestMapping("/web")
 @SessionAttributes("hotel")
 public class WebController {
-
+    /** Atributo statico para las trazas logger */
     static final Logger logger = Logger.getLogger(WebController.class.getName());
 
+    /** Inyeccion repository de hotel */
     @Autowired
     HotelRepository hotelRepository;
+    /** Inyeccion repository de poi */
     @Autowired
     PuntoInteresRepository puntoInteresRepository;
 
+    /**
+     * Metodo indexHoteles que devuelve en una plantilla html un listado de hoteles
+     * @param model model
+     * @return plantilla html tabla de todos los hoteles
+     */
     @GetMapping("/hoteles/")
     public String indexHoteles(Model model){
         var hoteles = hotelRepository.findAll();
@@ -33,6 +44,12 @@ public class WebController {
         return "indexHoteles";
     }
 
+    /**
+     * Metodo indexPuntoInteres que devuelve una plantilla html de un punto de interés turístico
+     * @param id id del poi
+     * @param model model
+     * @return plantilla html tarjeta con los datos de un poi por su id
+     */
     @GetMapping("/poi/{id}")
     public String indexPuntoInteres(@PathVariable String id, Model model){
         var poi= puntoInteresRepository.findById(id).get();
@@ -42,12 +59,23 @@ public class WebController {
         return "indexPoi";
     }
 
+    /**
+     * Metodo formularioAddHotel la vista del formulario para añadir un hotel a la DB
+     * @param model model
+     * @return formulario html
+     */
     @GetMapping("/hotel/add")
     public String formularioAddHotel(Model model){
         model.addAttribute("hotel", new Hotel());
         return "formularioHotel";
     }
 
+    /**
+     * Metodo addHotel que recoge el objeto Hotel del formulario y lo almacena en la BD
+     * @param hotel hotel
+     * @param model model
+     * @return regresa al html del formulario con un mensaje de que el hotel se ha añadido correctamente.
+     */
     @PostMapping("/hotel/add")
     public String addHotel(@ModelAttribute Hotel hotel, Model model) {
         hotelRepository.save(hotel);
