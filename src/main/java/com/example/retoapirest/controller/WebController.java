dbcf249200b867678d68bccb1ace0b69829e38f0
CarlosJavier <carlos.valenzuela.j92@gmail.com>
2025-03-05 19:55:12 +0100

    Commit - webController endpoint add hotel - html y css

--- src/main/java/com/example/retoapirest/controller/WebController.java
+++ src/main/java/com/example/retoapirest/controller/WebController.java
@@ -1,19 +1,23 @@
 package com.example.retoapirest.controller;
 
 
+import com.example.retoapirest.model.Hotel;
 import com.example.retoapirest.repository.EventoRepository;
 import com.example.retoapirest.repository.HotelRepository;
 import com.example.retoapirest.repository.PuntoInteresRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/web")
 public class WebController {
+
+    static final Logger logger = Logger.getLogger(WebController.class.getName());
 
     @Autowired
     HotelRepository hotelRepository;
@@ -35,6 +39,20 @@ public class WebController {
         model.addAttribute("poi", poi);
 
         return "indexPoi";
+    }
+
+    @GetMapping("/hotel/add")
+    public String formularioAddHotel(Model model){
+        model.addAttribute("hotel", new Object());
+        return "formularioHotel";
+    }
+
+    @PostMapping("/hotel/add")
+    public String addHotel(@ModelAttribute Hotel hotel, Model model) {
+        hotelRepository.save(hotel);
+        logger.log(Level.INFO, "Hotel añadido: "+hotel);
+        model.addAttribute("mensaje", "Hotel añadido correctamente");
+        return "formularioHotel";
     }
 
 }
